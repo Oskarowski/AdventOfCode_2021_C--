23b21fb40b42a60233a0d1724cddf81253f005d5
Oskarowski <oscar.kacprzak@gmail.com>
2022-12-08 22:47:13 +0100

    Part 2 Done

--- Day_9/day_9.cpp
+++ Day_9/day_9.cpp
@@ -6,7 +6,31 @@
 #include <string>
 using namespace std;
 
-void importHeightmap(vector<vector<int>> &heightmap, string &nameOfFile){
+struct haightmapNumber
+{
+    public:
+    int number;
+    bool ifchecked=false;
+
+    haightmapNumber(int numberFromImport){
+        this->number=numberFromImport;
+    }
+};
+
+struct lowPoint{
+    int xPos=0, yPos=0;
+
+    lowPoint(int x, int y){
+        this->xPos=x;
+        this->yPos=y;
+    }
+};
+
+vector<vector<haightmapNumber>> heightmap;
+vector<int> theLowestPoint;
+vector<lowPoint> lowPoints;
+
+void importHeightmap(string &nameOfFile){
     cout<<endl<<" importHeightmap";
     ifstream mapFile;
     mapFile.open(nameOfFile);
@@ -23,7 +47,8 @@ void importHeightmap(vector<vector<int>> &heightmap, string &nameOfFile){
             {
                 try
                 {
-                    heightmap[positionVertically].push_back((int)numberFromFile-48);  
+                    int number=(int)numberFromFile-48;
+                    heightmap[positionVertically].push_back(number);  
                     // cout<<" "<<heightmap[positionVertically][positionHorizontally];
                 }
                 catch(const std::exception& e)
@@ -39,76 +64,102 @@ void importHeightmap(vector<vector<int>> &heightmap, string &nameOfFile){
     }
 }
 
-void printHeightmap(vector<vector<int>> &heightmap){
+void printHeightmap(){
     for (int i = 0; i < heightmap.size(); i++)
     {
         for (int j = 0; j < heightmap[i].size(); j++)
         {
-            cout<<heightmap[i][j]<<" ";
+            cout<<heightmap[i][j].number<<" ";
         }
         cout<<endl;
     }
 }
 
-void searchIfTheLowest(vector<vector<int>> &heightmap, vector<int> &theLowestPoint){
+void searchIfTheLowest(){
     cout<<endl;
     for (int i = 0; i < heightmap.size(); i++)
     {
         for (int j = 0; j < heightmap[i].size(); j++)
         {
-            int localMin=heightmap[i][j];
+            int localMin=heightmap[i][j].number;
             if (i==0) //upper border
             {
                 //left and right upper corner
-                if(j==0 && (localMin<heightmap[i][j+1] && localMin<heightmap[i+1][j])) { theLowestPoint.push_back(localMin); }
-                if(j==heightmap[i].size()-1 && (localMin<heightmap[i][j-1] && localMin<heightmap[i+1][j])) { theLowestPoint.push_back(localMin); }
+                if(j==0 && (localMin<heightmap[i][j+1].number && localMin<heightmap[i+1][j].number)) { 
+                    theLowestPoint.push_back(localMin);
+                    lowPoints.emplace_back(j,i);
+                }
+                if(j==heightmap[i].size()-1 && (localMin<heightmap[i][j-1].number && localMin<heightmap[i+1][j].number)) { 
+                    theLowestPoint.push_back(localMin); 
+                    lowPoints.emplace_back(j,i);
+                }
                 //anythink in line other then corners
                 else
                 {
-                    if(localMin<heightmap[i][j-1] && localMin<heightmap[i][j+1] && localMin<heightmap[i+1][j]) { theLowestPoint.push_back(localMin); }
+                    if(localMin<heightmap[i][j-1].number && localMin<heightmap[i][j+1].number && localMin<heightmap[i+1][j].number) { 
+                        theLowestPoint.push_back(localMin); 
+                        lowPoints.emplace_back(j,i);
+                    }
                 }
 
             }
             else if (i==heightmap.size()-1) //bottom border
             {
                 //left and right bottom corner
-                if(j==0 && (localMin<heightmap[i][j+1] && localMin<heightmap[i-1][j])) { theLowestPoint.push_back(localMin); }
-                if(j==heightmap[i].size()-1 && (localMin<heightmap[i][j-1] && localMin<heightmap[i-1][j])) { theLowestPoint.push_back(localMin); }
+                if(j==0 && (localMin<heightmap[i][j+1].number && localMin<heightmap[i-1][j].number)) { 
+                    theLowestPoint.push_back(localMin); 
+                    lowPoints.emplace_back(j,i);
+                }
+                if(j==heightmap[i].size()-1 && (localMin<heightmap[i][j-1].number && localMin<heightmap[i-1][j].number)) { 
+                    theLowestPoint.push_back(localMin); 
+                    lowPoints.emplace_back(j,i);
+                }
                 //anythink in line other then corners
                 else
                 {
-                    if(localMin<heightmap[i][j-1] && localMin<heightmap[i][j+1] && localMin<heightmap[i-1][j]) { theLowestPoint.push_back(localMin); }
+                    if(localMin<heightmap[i][j-1].number && localMin<heightmap[i][j+1].number && localMin<heightmap[i-1][j].number) { 
+                        theLowestPoint.push_back(localMin);
+                        lowPoints.emplace_back(j,i);
+                    }
                 }
             }
             else if (j==0) //left border without corners
             {
-                if(localMin<heightmap[i-1][j] && localMin<heightmap[i][j+1] && localMin<heightmap[i+1][j]) { theLowestPoint.push_back(localMin); }
+                if(localMin<heightmap[i-1][j].number && localMin<heightmap[i][j+1].number && localMin<heightmap[i+1][j].number) { 
+                    theLowestPoint.push_back(localMin);
+                    lowPoints.emplace_back(j,i);
+                }
             }
             else if (j==heightmap[i].size()-1) //right border without corners
             {
-                if(localMin<heightmap[i-1][j] && localMin<heightmap[i][j-1] && localMin<heightmap[i+1][j]) { theLowestPoint.push_back(localMin); }
+                if(localMin<heightmap[i-1][j].number && localMin<heightmap[i][j-1].number && localMin<heightmap[i+1][j].number) { 
+                    theLowestPoint.push_back(localMin);
+                    lowPoints.emplace_back(j,i);
+                }
             }
             else //anythink what is not a border
             {
-                if(localMin<heightmap[i-1][j] && localMin<heightmap[i][j+1] && localMin<heightmap[i+1][j] && localMin<heightmap[i][j-1]) { theLowestPoint.push_back(localMin); }
+                if(localMin<heightmap[i-1][j].number && localMin<heightmap[i][j+1].number && localMin<heightmap[i+1][j].number && localMin<heightmap[i][j-1].number) { 
+                    theLowestPoint.push_back(localMin);
+                    lowPoints.emplace_back(j,i);
+                }
             }
         }
     }
 }
 
-void printLocalMin(vector<int> theLowestPoint){
+void printLocalMin(){
     cout<<endl<<" printLocalMin: "<<endl;
     for (auto &&i : theLowestPoint)
     {
         cout<<" "<<i;
     }
-    
 }
 
-void calculateTheRiskLevel(vector<int> lowestPoint, int &risk)
+void calculateTheRiskLevel(int &risk)
 {
-    cout<<endl<<" calculateTheRiskLevel: ";
-    for (auto &&i : lowestPoint)
+    // cout<<endl<<" calculateTheRiskLevel: ";
+    for (auto &&i : theLowestPoint)
     {
         i++;
         risk+=i;
@@ -117,29 +168,119 @@ void calculateTheRiskLevel(vector<int> lowestPoint, int &risk)
 
 void partOne(){
     cout<<endl<<"Part 1:";
-    vector<vector<int>> heightmap;
-    vector<int> theLowestPoint;
     int riskLevel=0;
-    string nameOfFile="day_9_input.txt";
 
-    importHeightmap(heightmap, nameOfFile);
     // printHeightmap(heightmap);
-    searchIfTheLowest(heightmap, theLowestPoint);
+    searchIfTheLowest();
     // printLocalMin(theLowestPoint);
-    calculateTheRiskLevel(theLowestPoint, riskLevel);
+    calculateTheRiskLevel(riskLevel);
 
     cout<<endl<<"The sum of the risk levels of all low points on your heightmap is: "<<riskLevel<<endl;
 }
 
+
+int whichIterationOfF=0, sizeOfBasin=0;
+void checkHowBigIsBasin(int x, int y){
+    whichIterationOfF++;
+    int centralNumber=heightmap[y][x].number;
+    if(centralNumber!=9)
+    {
+        // cout<<endl<<"centralNumber: "<<centralNumber<<" whichIterationOfF: "<<whichIterationOfF<<" sizeOfBasin: "<<sizeOfBasin;
+        sizeOfBasin++;
+        heightmap[y][x].ifchecked=true;
+        if(centralNumber==8 && heightmap[y][x].ifchecked==false){
+            // sizeOfBasin++;
+            heightmap[y][x].ifchecked=true;
+            }
+        if(y-1>=0){
+            if(heightmap[y-1][x].ifchecked==false && heightmap[y-1][x].number>centralNumber){
+                heightmap[y-1][x].ifchecked=true;
+                // cout<<" UP:"<<heightmap[y-1][x].number;
+                checkHowBigIsBasin(x, y-1);
+            }
+        }
+        if(x+1<heightmap[y].size()){
+            if(heightmap[y][x+1].ifchecked==false && heightmap[y][x+1].number>centralNumber){
+                heightmap[y][x+1].ifchecked=true;
+                // cout<<" RIGHT:"<<heightmap[y][x+1].number;
+                checkHowBigIsBasin(x+1, y);
+            }
+        }
+        if(y+1<heightmap.size()){
+            if(heightmap[y+1][x].ifchecked==false && heightmap[y+1][x].number>centralNumber){
+                heightmap[y+1][x].ifchecked=true;
+                // cout<<" DOWN"<<heightmap[y+1][x].number;
+                checkHowBigIsBasin(x, y+1);
+            }
+        }
+        if(x-1>=0){
+            if(heightmap[y][x-1].ifchecked==false && heightmap[y][x-1].number>centralNumber){
+                heightmap[y][x-1].ifchecked=true;
+                // cout<<" LEFT"<<heightmap[y][x-1].number;
+                checkHowBigIsBasin(x-1, y);
+            }
+        }
+    }
+}
+
+void goViaLowestCords(vector<int> &basinsSize){
+    for(int i = 0; i<lowPoints.size(); i++)
+    {
+        int x=lowPoints[i].xPos, y=lowPoints[i].yPos;
+        checkHowBigIsBasin(x,y);
+        basinsSize.push_back(sizeOfBasin);
+        sizeOfBasin=0;
+    }    
+}
+
+void sortBasins(vector<int> &basinsSize){
+    for (int i = 0; i < basinsSize.size(); i++)
+    {
+        for (int j = 0; j < basinsSize.size(); j++)
+        {
+            if(basinsSize[j]<basinsSize[j+1]) {
+                swap(basinsSize[j],basinsSize[j+1]);
+            }
+        }
+    }
+    
+}
+
+void printBasinsSize(vector<int> &basinsSize){
+    cout<<endl;
+    for (auto &&i : basinsSize)
+    {
+        cout<<" "<<i;
+    }
+    cout<<endl;
+}
+
+void partTwo(){
+    cout<<endl<<"Part 2:";
+    vector<int> basinsSize;
+    
+    goViaLowestCords(basinsSize);
+    printBasinsSize(basinsSize);
+    sortBasins(basinsSize);
+    printBasinsSize(basinsSize);
+    
+    cout<<endl<<"If you multiply together the sizes of the three largest basins you get: "<<basinsSize[0]*basinsSize[1]*basinsSize[2];
+
+}
+
+
 void letsRolling(){
 
     cout<<endl<<"Advent_of_Code_2021 Day 9: Smoke Basin";
-    partOne(); //working
+    string nameOfFile="day_9_input.txt";
+    importHeightmap(nameOfFile);
+
+    partOne(); //DONE
+    partTwo(); //DONE
 }
 
 
 int main(){
     letsRolling();
-
     return 0;
 }
